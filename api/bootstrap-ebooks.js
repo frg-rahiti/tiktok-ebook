8e90d0a13d48232bf64c90e8f61ef4a940d6db8a
FROGIER_Rahiti <142440811+frg-rahiti@users.noreply.github.com>
2026-09-02 08:44:16 +0200

    chore: remove temporary ebook storage bootstrap endpoint

--- api/bootstrap-ebooks.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const r = await fetch('https://mtjwxqymywachtkavifd.supabase.co/functions/v1/bootstrap-ebooks');
-  const text = await r.text();
-  res.status(r.status).setHeader('Content-Type','application/json').send(text);
-}
